3e4e92e9dfa027032c54139e44dc56b2dbdf6295
OneTwo345 <codeevil305@gmail.com>
2023-06-07 16:18:31 +0700

    Done bmi

--- ConditionAndLoopHomeWork/src/CountBMIOfBody.java
+++ ConditionAndLoopHomeWork/src/CountBMIOfBody.java
@@ -1,5 +1,25 @@
-public class CountBMIOfBody {
-    public static void main(String[]args){
+import java.util.Scanner;
 
+public class CountBMIOfBody {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double weight, height, bmi;
+        System.out.println("Input your weight (kg)");
+        weight = scanner.nextDouble();
+        System.out.println("Input your height (met)");
+        height = scanner.nextDouble();
+        bmi = weight / (height * height);
+        System.out.println("Your BMI: " + bmi);
+        if (bmi < 18.5) {
+            System.out.println("Underweight");
+        } else if (bmi < 25.0) {
+            System.out.println("Normal");
+        } else if (bmi < 30) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obese");
+        }
     }
+
 }
+
