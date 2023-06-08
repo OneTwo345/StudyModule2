9355a3425330cc16978e1d8ade9390a063db8d7f
OneTwo345 <codeevil305@gmail.com>
2023-06-08 11:54:41 +0700

    pst

---
+++ ConditionAndLoopHomeWork/src/ShowPrimeLowerThan100.java
@@ -0,0 +1,19 @@
+public class ShowPrimeLowerThan100 {
+    public static void main(String[] args) {
+        int number = 2;
+        System.out.println("2");
+        System.out.println("3");
+        while (number < 100) {
+            for (int i = 2; i < Math.sqrt(number); i++) {
+                if (number % i == 0) {
+                    break;
+                } else {
+                    System.out.println(number);
+                    break;
+                }
+            }
+            number++ ;
+        }
+    }
+
+}
